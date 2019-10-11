2c147ab2a28cdb56087f570298f34952ae06a571
Alex <wilkea21403@gmail.com>
2019-10-11 13:58:30 -0500

    Cleaning tree. Printout for dataframe in loadDatasheet method

--- data_preprocessing/seg.py
+++ data_preprocessing/seg.py
@@ -4,7 +4,7 @@ Basic script used to preprocessing data and feature extraction
 @author: Aydan Jesson, Alex Wilke
 """
 
-import pandas as pdb
+import pandas as pd
 import pdb
 import os
 
@@ -12,11 +12,11 @@ import os
 def loadDataset(filepath):
     ##Loads dataset from CSV
     dfData = pd.read_csv(filepath)
-
+    print(dfData)
 
 def main():
     cwd = os.getcwd()
-    filename = r'datain\samples.csv'
+    filename = r'/datain/samples.csv'
     
     def init():
         pass
