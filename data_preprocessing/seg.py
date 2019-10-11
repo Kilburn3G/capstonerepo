044b21c5c2416dd7ec71e4dd4f585974eec7de14
Alex <wilkea21403@gmail.com>
2019-10-11 13:23:52 -0500

    Added loadDataset function for csv.

--- data_preprocessing/seg.py
+++ data_preprocessing/seg.py
@@ -4,11 +4,25 @@ Basic script used to preprocessing data and feature extraction
 @author: Aydan Jesson, Alex Wilke
 """
 
-def main():
+import pandas as pdb
+import pdb
+import os
 
+
+def loadDataset(filepath):
+    ##Loads dataset from CSV
+    dfData = pd.read_csv('data.csv')
+
+
+def main():
+    cwd = os.getcwd()
+    filename = '100.csv'
+    
     def init():
         pass
 
     init()
+    
+    loadDataset(cwd + filename)
 
 main()
