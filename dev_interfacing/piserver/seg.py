2ffc384014633eb749932eeb62d7dfc1857e8a49
Alex <wilkea21403@gmail.com>
2020-01-20 12:37:06 -0600

    SEG Code modularized

--- dev_interfacing/piserver/seg.py
+++ dev_interfacing/piserver/seg.py
@@ -6,10 +6,30 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 
-V = data.loc[1:1000]
+data = pd.read_csv('samples_102.csv',index_col=None)
+data = data.apply(pd.to_numeric,errors='coerce')
+V = data.iloc[1:1000,1]
 V.index = np.arange(1,len(V)+1)
     
 
+def getPeaks(V,E,A):
+
+    E_thresh = 0.01;
+    A_thresh = -0.002;
+
+    peaks = []
+
+    for center in range(2,len(E-2)):
+        C = E[center]
+        C1 = E[center-1]
+        C2 = E[center-2]
+        
+        if C2 > C1 and C1 < C and A[center] < A_thresh:
+
+            peaks += [center]
+
+    print('Peaks found : %d' %len(peaks))
+    return peaks
 
 
 def processSamples(V):
@@ -43,32 +63,27 @@ def processSamples(V):
         E_vector = (1.0 / window_size) * np.sum(np.power((V_window- V_prime),2))
 
         E[window_center] = float(E_vector);
-        A[window_center] = float(a);     
+        A[window_center] = float(a);    
+
+    return E, A
+
+def plotSegments(V,peaks):
 
 
-E_thresh = 0.01;
+    avg_samples = np.sum(np.diff(peaks))/len(np.diff(peaks))
+    print('Average Samples between peaks : %d' %avg_samples)
 
-A_thresh = -0.002;
-
-peaks = []
-
-for center in range(2,len(E-2)):
-    C = E[center]
-    C1 = E[center-1]
-    C2 = E[center-2]
-    
-    if C2 > C1 and C1 < C and A[center] < A_thresh:
-
-        peaks += [center]
+    plt.hold(True)
+    for i in range(1,len(V),avg_samples):    
+        ax.plot(V.loc[i:i+avg_samples])
+    plt.show()
+        
+    plt.hold(False)
 
 
-avg_samples = np.sum(np.diff(peaks))/len(np.diff(peaks))
 
-print('Peaks found : %d' %len(peaks))
-print('Average Samples between peaks : %d' %avg_samples)
+
+E , A = processSamples(V)
+peaks = getPeaks(V,E,A)
 pdb.set_trace()
-plt.hold(True)
-
-for i in range(1,len(V),avg_samples):    
-    ax.plot(V.loc[i:i+avg_samples])
-plt.show()
+plotSegments(V,peaks)
