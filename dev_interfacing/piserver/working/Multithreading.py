443db90ef89329e6cfcb07f7d97e755b55b0a76c
wilkea <wilkea21403@gmail.com>
2020-01-23 18:15:44 -0600

    basics

--- dev_interfacing/piserver/working/Multithreading.py
+++ dev_interfacing/piserver/working/Multithreading.py
@@ -2,9 +2,29 @@ import threading
 import pdb
 import numpy as np
 import pandas as pb
+import multiprocessing
 
+def worker1():
+    
+    print ('Worker1')
+    return
+    
+def worker2():
+    
+    print ('Worker2')
+    return
 
-def main():
-    pass
+if __name__ == '__main__':
+    jobs = []
+    
 
-main()
+    for i in range(50):
+        p1 = multiprocessing.Process(target=worker1)
+        p2 = multiprocessing.Process(target=worker2)
+        
+        p1.start()
+        p2.start()
+
+    print("Joined")
+    p1.join()
+    p2.join()
